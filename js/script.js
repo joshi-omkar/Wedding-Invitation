7f3c2f92b40a00081491a7f0ffd2b34ee2fe7fec
omkar <Omkar Joshi>
2022-11-23 22:47:54 +0530

    done

--- js/script.js
+++ js/script.js
@@ -20,12 +20,12 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
-// $(document).ready('click', function(){
-//     document.getElementById("my_audio").play();
-//     console.log('Shaadi me zaroor aana');
-// });
+$(document).on('click', function(){
+    document.getElementById("my_audio").play();
+    console.log('Shaadi me zaroor aana');
+});
 
-window.onload = function() { document.getElementById("my_audio").play(); }
+// window.onload = function() { document.getElementById("my_audio").play(); }
 
 // Set the date we're counting down to
 var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
